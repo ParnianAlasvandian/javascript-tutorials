d0958448e21cfceeb2e77c34f03e4cf6febd3c73
Parnian Alasvandian <parnian.alasvandian@gmail.com>
2023-08-23 19:28:24 +0430

    Conditional operators

---
+++ lesson09/index.js
@@ -0,0 +1,15 @@
+console.log(" 5 > 1 : ", 5 > 1 );
+console.log(" 1 < 4 : ", 1 < 4 );
+console.log(" 5 <= 5 : ", 5 <= 5 );
+console.log(" 5 >= 5 : ", 5 >= 5 );
+console.log(" 5 < 5 : ", 5 < 5 );
+console.log(" 5 > 5 : ", 5 > 5 );
+
+console.log(" 5 == 5 : ", 5 == 5 );
+console.log(" 5 == '5' : ", 5 == "5" );
+console.log(" 5 === '5' : ", 5 === "5" );
+
+console.log(" 5 != '5' : ", 5 != "5" );
+console.log(" 5 !== '5' : ", 5 !== "5" );
+ 
+ 
