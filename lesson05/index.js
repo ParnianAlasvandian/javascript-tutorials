bcc3c63a89080de5f2245dca2cabc9ee74a66881
Parnian Alasvandian <parnian.alasvandian@gmail.com>
2023-08-23 19:19:19 +0430

    Strings in javascript

---
+++ lesson05/index.js
@@ -0,0 +1,13 @@
+let str1 = 'this is a string' ;
+let str2 = "this is a different string" ;
+let str3 = `this is another string` ;
+let str4 = "true" ;
+let str5 = "123432" ;
+let str6 = 'false' ;
+let message = 'Hi there i\'m Parnian Alasvandian, I live in "Tehran" and js street' ;
+let username = "ParnianAlasvandian" ;
+let age = 21;
+let idcard = 1960684426 ;
+let welcomeMSG = `Hello ${username} welcome to our website, you were born in ${2023-age} and your IDCard is ${idcard}`;
+console.log(welcomeMSG);
+ 
