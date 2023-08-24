2fc29f7759e6c04a82da1c95e808e4bbe30d0cfe
Parnian Alasvandian <parnian.alasvandian@gmail.com>
2023-08-24 05:19:57 +0430

    Naming variables in JavaScript

---
+++ lesson12/index.js
@@ -0,0 +1,19 @@
+let nationalId;
+let NationalID;
+let national_id;
+let username;
+let userName;
+let myUserName ;
+let user_name; 
+let user_Name;
+let UserName;
+let age;
+let Age;
+let AGE;
+
+// kabab-case
+//snack_case
+//camelCase
+//PaskalCase
+//UPERCASE
+//lowercase
