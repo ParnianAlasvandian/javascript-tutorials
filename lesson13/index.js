4e56e34412033d8f116b43100ac96a7b828100d7
Parnian Alasvandian <parnian.alasvandian@gmail.com>
2023-08-24 05:21:30 +0430

    Switch case

---
+++ lesson13/index.js
@@ -0,0 +1,49 @@
+let username = "Parnian" ;
+switch (username) {
+    case "Eli":
+        console.log("this is Eli");
+        break;
+    case "Yas" :
+        console.log("this is Yas");
+        break;
+    case "Aria" :
+        console.log("this is Aria");
+        break;
+                
+    case "Parvin" :
+        console.log("this is Parvin");
+        break;
+
+    default:
+        console.log("I don't know him.  ");
+        break;
+}
+
+const day = 0 ;
+switch (day) {
+    case 0:
+        console.log("Saturday");
+        break;
+    case 1:
+        console.log("Sunday");
+        break
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+
+    default:
+        console.log("inValid day number");
+        break;
+}
