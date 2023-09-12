85ece7eb5778c71bb047b388e437e8db115f60c5
Parnian Alasvandian <parnian.alasvandian@gmail.com>
2023-09-12 18:03:10 +0430

    Hoisting

---
+++ lesson38/index.js
@@ -0,0 +1,67 @@
+// Hoisting
+
+Nodejs () ;
+//Func () ;
+//Func2 () ;
+myFunc () ;
+console.log(programmingLanguage);
+
+function Nodejs() {
+    console.log("Nodejs course");
+}
+
+var programmingLanguage = "javaScript" ;
+//
+const Func = () => {
+    console.log("Some task...");
+}
+//
+function Func2 () {
+    console.log("Some task2...");
+}
+//
+const myFunc = function() {
+    console.log("Some task3...");
+}
+
+
+
+
+
+
+x = 10 ;
+myFunction () ;
+function myFunction () {
+    //do something ...
+}
+var x = 20 ;
+
+
+
+
+
+
+
+
+/*
+var x ;
+function myFunction () {
+    // do some thing ...
+}
+x = 10 ;
+myFunction() ;
+x = 20 ;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
